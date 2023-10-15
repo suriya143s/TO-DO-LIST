4ea8616ba752d5b1ac957b67a778d79b51a14efc
suriya143s <127228229+suriya143s@users.noreply.github.com>
2023-10-15 22:28:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const [showEdit, setShowEdit] = useState(-1);
+  const [updatedText, setUpdatedText] = useState("");
+
+    function addItem() {
+  
+    if (!newItem) {
+      alert("Press enter an item.");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+
+   
+    setItems((oldList) => [...oldList, item]);
+
+    setNewItem("");
+  }
+
+    function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
+ 
+  function editItem(id, newText) {
+
+    const currentItem = items.filter((item) => item.id === id);
+
+    const newItem = {
+      id: currentItem.id,
+      value: newText,
+    };
+
+    deleteItem(id);
+
+    setItems((oldList) => [...oldList, newItem]);
+    setUpdatedText("");
+    setShowEdit(-1);
+  }
+
+  return (
+    <div className="app">
+      {/* 1. Header  */}
+      <h1>My Todo List</h1>
+
+      {/* 2. Add new item (input) */}
+      <input
+        type="text"
+        placeholder="Add an item..."
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+
+      {/* Add (button) */}
+      <button onClick={() => addItem()}>Add</button>
+
+      {/* 3. List of todos (unordered list) */}
+      <ul>
+        {items.map((item) => {
+          return (
+            <div>
+              <li key={item.id} onClick={() => setShowEdit(item.id)}>
+                {item.value}
+                <button
+                  className="delete-button"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  ❌
+                </button>
+              </li>
+
+              {showEdit == item.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={updatedText}
+                    onChange={(e) => setUpdatedText(e.target.value)}
+                  />
+                  <button onClick={() => editItem(item.id, updatedText)}>
+                    Update
+                  </button>
+                </div>
+              ) : null}
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
